ca8a1eabc6e334684fd02c6b20c2eaa75a048c98
PapaAnam <hairulanam21@outlook.com>
2018-10-29 08:34:30 +0700

    edit generate until month

--- database/seeds/GenerateAttendanceUntilEndMonth.php
+++ database/seeds/GenerateAttendanceUntilEndMonth.php
@@ -13,19 +13,30 @@ class GenerateAttendanceUntilEndMonth extends Seeder
      */
     public function run()
     {
-        $employees = Employee::active();
-        $tglMaks = date('t');
-        $hariIni = date('d');
-        foreach ($employees as $e) {
-        	foreach (range($hariIni, $tglMaks) as $tgl) {
-        		Attendance::updateOrCreate([
-        			'created_at'=>date('Y-m').'-'.$tgl,
-        			'employee'=>$e->id,
-        		],[
-                    'enter'=>'08:30:00',
-                    'out'=>'17:00:00'
-                ]);
-        	}
-        }
+    	$employees = Employee::active();
+    	$tglMaks = date('t');
+    	$hariIni = date('d');
+    	foreach ($employees as $e) {
+    		foreach (range($hariIni, $tglMaks) as $tgl) {
+    			$data = [
+    				'enter'=>'08:30:00',
+    				'out'=>'17:00:00',
+    				'break'=>'12:00:00',
+    				'end_break'=>'13:00:00',
+    			];
+    			if(date('N',strtotime(date('Y-m').'-'.$tgl)) == 7){
+    				$data = [
+    					'enter'=>null,
+    					'out'=>null,
+    					'break'=>null,
+    					'end_break'=>null,
+    				];
+    			}
+    			Attendance::updateOrCreate([
+    				'created_at'=>date('Y-m').'-'.$tgl,
+    				'employee'=>$e->id,
+    			],$data);
+    		}
+    	}
     }
 }
